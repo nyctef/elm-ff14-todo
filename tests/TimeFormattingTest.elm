4eb80e3195fd8473bffc9b62d11855de1f499daa
Nyctef <nyctef@nyctef.com>
2022-03-08 11:15:24 +0000

    use <| to avoid some parens in test

--- tests/TimeFormattingTest.elm
+++ tests/TimeFormattingTest.elm
@@ -3,12 +3,12 @@ module TimeFormattingTest exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
-import Time exposing (utc, Month(..))
-import TimeFormatting exposing (..)
+import Time exposing (Month(..), utc)
 import Time.Extra exposing (Parts, partsToPosix)
+import TimeFormatting exposing (..)
 
 
 suite : Test
 suite =
-    test "times are formatted with leading zeros"
-        (\_ -> Expect.equal "01:02:03" (formatPosixTime utc (partsToPosix utc (Parts 2020 Jan 20 1 2 3 99))))
+    test "times are formatted with leading zeros" <|
+        \_ -> Expect.equal "01:02:03" (formatPosixTime utc <| partsToPosix utc <| Parts 2020 Jan 20 1 2 3 99)
