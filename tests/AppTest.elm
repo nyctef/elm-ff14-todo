c0a4a91c9c949ff8c0af61f18e2cc69591529081
Nyctef <nyctef@nyctef.com>
2022-03-09 11:01:16 +0000

    some awkward syntax for doing multiple assertions on one object

--- tests/AppTest.elm
+++ tests/AppTest.elm
@@ -1,20 +1,37 @@
 module AppTest exposing (..)
 
-import App exposing (update, init, Msg(..))
+import App exposing (Msg(..), init, update)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
-import Time exposing (utc)
+import Time exposing (Month(..), utc)
+import Time.Extra exposing (Parts, partsToPosix)
 
--- throw away commands since we can't handle them in tests
-dumbInit = init >> Tuple.first
-dumbUpdate x = update x >> Tuple.first
+
+dumbInit =
+    -- throw away commands since we can't easily handle them in tests
+    init >> Tuple.first
+
+
+dumbUpdate x =
+    -- throw away commands since we can't easily handle them in tests
+    update x >> Tuple.first
+
 
 suite =
     describe "update function"
         [ test "setting the initial model" <|
             let
-                model = dumbInit () |> dumbUpdate (SetTz utc) 
+                now =
+                    partsToPosix utc <| Parts 2020 Jan 20 1 2 3 99
+
+                model =
+                    dumbInit () |> dumbUpdate (SetTz utc) |> dumbUpdate (Tick now)
             in
-            \_ -> Expect.equal utc model.myTz
+            \_ ->
+                Expect.all
+                    [ .myTz >> Expect.equal utc
+                    , .instant >> Expect.equal now
+                    ]
+                    model
         ]
